b1e5f3781e96c3a70abb6aac7d5985b5af7490c5
Azat Jalilov <azat777@yandex.ru>
2018-10-26 17:49:36 +0200

    add other params

--- services/slack-service.js
+++ services/slack-service.js
@@ -4,20 +4,22 @@ function parseSlackRequest(ctx) {
     const request = ctx.request.url;
     const requstParams = querystring.parse(request);
     const command = requstParams.command;
+    const responseUrl = requstParams.responseUrl;
+    const userId = requstParams.user_id;
     const text = requstParams.text;
     if (!text) {
-        throw 'Invalid arguments';
+        return 'Please specify text arguments';
     }
     const variables = text.split(' ');
     let payload = {};
     variables.forEach(variable => {
         const arg = variable.split(':');
         if (arg.length !== 2) {
-            throw 'Invalid arguments';
+            return 'Invalid arguments';
         }
         payload[arg[0]] = arg[1];
     });
-    return { command, payload };
+    return { command, payload, responseUrl, userId };
 }
 
 function formatResponse(message) {
