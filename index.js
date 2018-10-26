6c9502044b7b9754b24981850a10b562c77478fb
doga.budak <doga.budak@check24.de>
2018-10-26 15:07:41 +0200

    initial

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * Created by doga on 22/10/2016.
+ */
+
+//const config = require('./resources/config.js');
+
+const googleMapsClient = require('@google/maps').createClient({
+  key: 'AIzaSyBO7-7iiJAhph0kVbJ9wp4N95t1PBRVVUU'
+});
+const mongoose = require('mongoose');
+
+
+
+const Koa = require('koa');
+const koaApp = new Koa();
+koaApp.use(async (ctx, next) => {
+
+
+  ctx.status = 200
+  ctx.body = "hello";
+  next()
+
+});
+koaApp.listen(3111);
