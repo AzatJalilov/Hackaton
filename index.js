816e56b2ecec510a87156d6b215025a352e6adf1
Kerem Bıçakcıoğlu <wulfricin@Kerems-MacBook-Pro.local>
2018-10-27 12:05:22 +0200

    Add pagination for restaurant fetch Add details method

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const slackService = require('./services/slack-service');
 const recommendationService = require('./services/recommendation-service');
 const Router = require('koa-router');
 const Koa = require('koa');
-const { getPlacesList } = require('./services/google-maps-service.js');
+const { getPlacesList, getPlaceDetail } = require('./services/google-maps-service.js');
 
 const app = new Koa();
 const router = new Router();
@@ -18,9 +18,25 @@ app
   .use(router.allowedMethods());
 
 router.get('/lucky', async (ctx, next) => {
-  const queryString = (ctx.request.query && ctx.request.query.qs) ? ctx.request.query.qs : "fast food"
-  let restaurants = await getPlacesList(queryString);
-  restaurants.forEach(restaurant => restaurantsStore.insertRestaurants(restaurant));
+  const queryString = (ctx.request.query && ctx.request.query.qs) ? ctx.request.query.qs : "fast food";
+  restaurantsStore.getToken().then(async (nextToken) => {
+    let { restaurants, token } = await getPlacesList(queryString, nextToken);
+    restaurants.forEach(restaurant => restaurantsStore.insertRestaurants(restaurant));
+    restaurantsStore.saveToken(token);
+  });
+  ctx.status = 200;
+  next();
+});
+
+router.get('/fillmeup', async (ctx, next) => {
+  restaurantsStore.getRestaurants()
+    .then((restaurants) => 
+      restaurants.forEach(async (currentRestaurant) => {
+        console.log(currentRestaurant);
+        const restaurantDetail = await getPlaceDetail(currentRestaurant.placeId);
+        return restaurantsStore.updateRestaurantDetails(restaurantDetail);
+      })
+  );
   ctx.status = 200;
   next()
 });
--- services/google-maps-service.js
+++ services/google-maps-service.js
@@ -2,20 +2,27 @@ const config = require('../resources/config.js');
 
 const googleMapsClient = require('@google/maps').createClient({
   key: config.googleApi.key,
+  rate: {limit: 50},
   Promise: Promise
 });
-
-async function getPlacesList(query) {
-  const places = await googleMapsClient.places({
-      query: query,
-      location: [config.googleApi.locationAlt, config.googleApi.locationLet],
-      radius: 1000,
-      minprice: 0,
-      maxprice:4,
-      type: 'restaurant'
-    }).asPromise();
-    console.log(places);
-    return places.json.results;
+async function getPlacesList(query, nextPageToken) {
+  const options = {
+    query: query,
+    location: [config.googleApi.locationAlt, config.googleApi.locationLet],
+    radius: 1000,
+    minprice: 0,
+    maxprice:4,
+    type: 'restaurant'
+  };
+  if(nextPageToken && nextPageToken.token) {
+    options.pagetoken = nextPageToken.token;
+  }
+  const places = await googleMapsClient.places(options).asPromise();
+  return { restaurants : places.json.results, token: places.json.next_page_token };
+}
+async function getPlaceDetail(placeId) {
+  const placeDetailResponse = await googleMapsClient.place({ placeid: placeId }).asPromise();
+  return placeDetailResponse.json.results;
 }
 
-module.exports = { getPlacesList };
+module.exports = { getPlacesList, getPlaceDetail };
